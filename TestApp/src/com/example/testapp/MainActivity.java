e902038841accc974bb4570f2cf2658514c30328
svetzal <svetzal@mojility.ca>
2014-06-25 09:42:20 -0400

    Added a todo for later

--- TestApp/src/com/example/testapp/MainActivity.java
+++ TestApp/src/com/example/testapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+	// TODO: My comment
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
